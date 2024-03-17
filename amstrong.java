2e7076f35268cbd1929395e191361fb87790864a
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-03-17 07:22:16 +0530

    Amstrrong number 1 to 1000 2

--- amstrong.java
+++ amstrong.java
@@ -21,3 +21,35 @@ public class amstrong
         }
     }
 }
+
+
+// second armstrong
+
+// armstrong between 1 to 1000
+
+public class amstrong
+{
+    public static void main(String args[])
+    {
+        int num,number,temp,total;
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("Enter the value :");
+        // num = s.nextInt();
+        for(num=1;num <=1000;num++)
+        {
+            number = num;
+            temp = 0;
+            total = 0;
+            while(number != 0)
+            {
+                temp = number % 10;
+                total = total+temp*temp*temp;
+                number = number / 10;
+            }
+            if(total == num)
+            {
+                System.out.println(num);
+            }
+        }
+    }
+}
