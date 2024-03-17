71f55c298e68791038eb182159468ff3dc31c6df
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-03-17 07:23:14 +0530

    fctorial using recursion  2

--- factorial.java
+++ factorial.java
@@ -22,3 +22,30 @@ public class factorial
         return output;
     }
 }
+
+// factorial 2
+
+// factorial
+import java.util.Scanner;
+
+public class factorial
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the value");
+       int num = s.nextInt();
+        int factorial = fact(num);
+        
+        System.out.println("the factoial value is:" + factorial);
+    }
+    static int fact(int n)
+    {
+        int output;
+        if(n == 1){
+            return 1; 
+        }
+        output = fact(n-1)*n;
+        return output;
+    }
+}
